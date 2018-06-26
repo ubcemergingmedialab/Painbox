ea58b160efdd4b67655c0ae446b26bcc39445918
William Beltran <williambeltran1997@gmail.com>
2018-06-26 16:25:06 -0700

    "Adding colliders to sphere and knife mesh, knife now makes sphere disappear"

---
+++ Painbox 2018.1.4/Assets/Scripts/KnifeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeBehaviour : MonoBehaviour {
+
+    private MeshCollider KnifeCollider;
+    private Vector3 KnifePosition;
+
+
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Balloon")
+        {
+            print("Testing");
+            Destroy(other.gameObject);
+        }
+    }
+}
