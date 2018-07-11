2b245a816b12755af156ffe817ebf9ef379d01e2
William Beltran <williambeltran1997@gmail.com>
2018-07-10 17:02:15 -0700

    Modified the collision code

--- Painbox 2018.1.4/Assets/Scripts/KnifeBehaviour.cs
+++ Painbox 2018.1.4/Assets/Scripts/KnifeBehaviour.cs
@@ -6,16 +6,49 @@ public class KnifeBehaviour : MonoBehaviour {
 
     private MeshCollider KnifeCollider;
     private Vector3 KnifePosition;
+    
+    private Rigidbody knifeBody;
+    private Texture2D OtherTexture;
+    private ContactPoint[] pointsOfContactWithSphere;
 
 
-
+    private void Start()
+    {
+        if(knifeBody.velocity.x > 0)
+        {
+            print("MOvement!");
+        }
+    }
     // Update is called once per frame
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
+    {
+        print(other.collider.gameObject.tag);
+        if (other.collider.gameObject.tag == "Balloon")
+        {
+            print("Before the for loop");
+            pointsOfContactWithSphere = other.contacts; // getting the points of contact of the collision
+           
+            OtherTexture = (Texture2D)other.collider.gameObject.GetComponent<Renderer>().material.mainTexture;
+
+            for(int i = 0; i < pointsOfContactWithSphere.Length; i++)
+            {
+                //print(pointsOfContactWithSphere[i].normal.x);
+                Debug.DrawRay(pointsOfContactWithSphere[0].point, pointsOfContactWithSphere[0].normal, Color.white,5,false);
+               // OtherTexture.SetPixel((int)pointsOfContactWithSphere[i].point.x, (int)pointsOfContactWithSphere[i].point.y, new Color(0,0,0));
+            }
+
+            print("Inside for loop");
+            //Destroy(other.collider.gameObject);
+        }
+        print("Testing");
+    }
+
+   /* private void OnTriggerEnter(SphereCollider other)
     {
         if (other.tag == "Balloon")
         {
             print("Testing");
-            Destroy(other.gameObject);
+            other.gameObject.GetComponent<Renderer>().material = Poke;
         }
-    }
+    }*/
 }
