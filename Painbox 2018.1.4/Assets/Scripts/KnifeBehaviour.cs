112d17b596ffa3aeba8e176c816666e7c83575cb
William Beltran <williambeltran1997@gmail.com>
2018-07-18 13:19:54 -0700

    Testing out a hand model on Vuforia

--- Painbox 2018.1.4/Assets/Scripts/KnifeBehaviour.cs
+++ Painbox 2018.1.4/Assets/Scripts/KnifeBehaviour.cs
@@ -75,21 +75,9 @@ public class KnifeBehaviour : MonoBehaviour {
          }
      }*/
 
-    public void OnCollisionEnter(Collision collision)
-
-    {
+    
 
 
-        RaycastHit hit = new RaycastHit();
-        Ray ray = new Ray(collision.contacts[0].point - collision.contacts[0].normal, collision.contacts[0].normal);
-        if (Physics.Raycast(ray, out hit))
-        {
-
-            print("Testing");
-            other.gameObject.GetComponent<Renderer>().material = Poke;
-
-
-            print(hit.textureCoord);
-        }
+     
     }
-}
+
