2068ad453a862a00d630fe91f66ec4510281ef25
wbeltran777 <williambeltran1997@gmail.com>
2018-07-19 15:48:52 -0700

    Started working on UI, splash screen transitions to main menu

---
+++ Painbox 2018.1.4/Assets/Scripts/BalloonBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonBehaviour : MonoBehaviour {
+
+   /* public void OnCollisionEnter(Collision collision)
+    {
+
+
+        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(collision.contacts[0].point - collision.contacts[0].normal, collision.contacts[0].normal);
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            print(hit.textureCoord);
+        }
+    }*/
+}
